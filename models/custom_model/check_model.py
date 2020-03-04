8f8da6998f2d83fc162d3acd65477409f5b11bee
Deltoidex <rogerbm26@gmail.com>
2020-03-04 22:49:32 +0100

    pequeño script para comnprovar los modelos entrenados

---
+++ models/custom_model/check_model.py
@@ -0,0 +1,51 @@
+import spacy, timeit
+
+
+class NERFrameworksTest:
+
+    def SPACY(self, corpus, spacy_nlp):
+
+        list_results_spacy = list()
+        list_of_indices = list()
+
+        document = spacy_nlp(corpus)
+        # for token in document:
+        #     print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+        #         token.shape_, token.is_alpha, token.is_stop)
+
+        for ent in document.ents:
+            # print(ent.text, ent.start_char, ent.end_char, ent.label_)
+            if ent.label_ == 'FECHA':
+                list_results_spacy.append(ent.text)
+                # print(ent.text, ent.label_)
+        return list_results_spacy
+
+
+
+def mainTest():
+
+    """CASTELLANO"""
+
+    f = open("C:/Users/Usuario/PycharmProjects/tfg-nlp/documents/ej_2_es_cleaned.txt", encoding="utf8")
+    corpus = f.read()
+
+
+    # Orquestation
+    ner = NERFrameworksTest()
+
+    # LOAD SPACY
+    t1 = timeit.default_timer()
+    # es_core_news_sm
+    spacy_nlp = spacy.load('es_core_legal_sm')
+    spacy_nlp.max_length = 2000000
+    t2 = timeit.default_timer()
+
+    print(f"Time to load Own Model: {t2-t1}")
+
+    result_Spacy = ner.SPACY(corpus, spacy_nlp)
+    for i in result_Spacy:
+        print(i)
+
+if __name__ == '__main__':
+
+    mainTest()
