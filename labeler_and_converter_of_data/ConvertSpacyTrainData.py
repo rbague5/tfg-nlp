6fe85a00ef00c5ba4cb1c844fa1f7e4909c7ab6f
Deltoidex <rogerbm26@gmail.com>
2020-03-09 19:02:41 +0100

    Ya no crea fichero txt, hace return de la lista de train data

--- labeler_and_converter_of_data/ConvertSpacyTrainData.py
+++ labeler_and_converter_of_data/ConvertSpacyTrainData.py
@@ -2,24 +2,24 @@
 # -*- coding: utf-8 -*-
 import json, io
 
-filename = input("Enter your train data filename : ")
-print(f"Converting: {filename} to spaCy Training Data")
 
+def convert_data(filename="data.json"):
 
-with io.open(filename, encoding='utf-8') as train_data:
-    train = json.load(train_data)
+    with io.open(filename, encoding='utf-8') as train_data:
+        train = json.load(train_data)
 
-TRAIN_DATA = []
-for data in train:
-    ents = [tuple(entity) for entity in data['entities']]
-    # print(ents)
-    # print(f"({data['content']}, 'entities': {ents})")
-    TRAIN_DATA.append((data['content'], {'entities': ents}))
+    TRAIN_DATA = []
+    for data in train:
+        ents = [tuple(entity) for entity in data['entities']]
+        # print(ents)
+        # print(f"({data['content']}, 'entities': {ents})")
+        TRAIN_DATA.append((data['content'], {'entities': ents}))
 
+    # with open('{}'.format(filename.replace('json', 'txt')), 'w', encoding='utf-8') as write:
+    #     write.write(str(TRAIN_DATA))
+    # print("Created new Training Data file in current directory")
+    return TRAIN_DATA
 
-with open('{}'.format(filename.replace('json', 'txt')), 'w', encoding='utf-8') as write:
-    write.write(str(TRAIN_DATA))
-print("Created new Training Data file in current directory")
-
-
+if __name__ == "__main__":
+    convert_data()
 
