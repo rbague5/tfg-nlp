7313c2f8777c98d358c84d27f36069a3761ecce9
Deltoidex <rogerbm26@gmail.com>
2020-06-26 17:02:27 +0200

    Updated algorith to test models

--- models/custom_model/CustomModel.py
+++ models/custom_model/CustomModel.py
@@ -1,5 +1,6 @@
 import timeit
 
+import spacy
 from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktParameters
 
 class CustomModel:
@@ -49,21 +50,45 @@ class CustomModel:
 
 		return sentences
 
-	def execute_core_algorithm(self, corpus):
-		pass
+	def execute_core_algorithm(self, corpus, model):
+		# load spacy model
+		t1 = timeit.default_timer()
+		# es_core_news_sm
+		spacy_nlp = spacy.load(model)
+		spacy_nlp.max_length = 2000000
+		t2 = timeit.default_timer()
+
+		print(f"Time to load Own Model: {t2 - t1}")
+		list_results_spacy = list()
+		list_of_indices = list()
+
+		document = spacy_nlp(corpus)
+		# for token in document:
+		#     print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+		#         token.shape_, token.is_alpha, token.is_stop)
+
+		for ent in document.ents:
+			# print(ent.text, ent.start_char, ent.end_char, ent.label_)
+			if ent.label_ == 'FECHA':
+				list_results_spacy.append(ent.text)
+				# print(f"'{ent.text}-{ent.start_char}:{ent.end_char}',")
+			# print(i)
+			# print(ent.text, ent.label_)
+		return list_results_spacy
 
 def main_custom_model():
 
-	f = open("../../documents/STS_175_2020.txt", "r", encoding="utf8")
+	f = open("../../documents/ej_2_es_cleaned.txt", "r", encoding="utf8")
 	file = f.read()
 
 	# Orquestation
 	custom_model = CustomModel()
+	model_name = "es_core_legal_sm"
 
 	corpus_preprocessed = custom_model.execute_pre_process(file)
 
 	t1 = timeit.default_timer()
-	result_custom_model = custom_model.execute_core_algorithm(corpus_preprocessed)
+	result_custom_model = custom_model.execute_core_algorithm(corpus_preprocessed, model_name)
 	t2 = timeit.default_timer()
 	print(f"Total time for Custom Model algorithm {t2-t1}")
 
